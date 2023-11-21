5860f7ad2e2972c939670bad241bcccdfa58ee01
GaneshVtv <ganesh.nainarofficial@gmail.com>
2023-11-21 13:54:13 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+module.exports = {
+  // other configurations...
+  theme: {
+    extend: {
+      backgroundColor: {
+        'custom': '#ff9900', // Add your custom background color value
+
+      },
+    },
+  },
+}
